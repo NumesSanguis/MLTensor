fffd16847f0630b944da1fe30b52f0728bb15421
robin6505 <robin.koezen@gmail.com>
2015-12-10 16:26:50 +0100

    csv read

--- adience/adience_input.py
+++ adience/adience_input.py
@@ -2,6 +2,7 @@ import tensorflow.python.platform
 import tensorflow as tf
 
 from os.path import join
+import csv
 
 
 def read_adience(folder_queue):
@@ -13,10 +14,30 @@ def read_adience(folder_queue):
     test[2][0] = ['64504106@N06', '11831304783_488d6c3a6d_o.jpg', 'm']
     test[3][0] = ['113445054@N07', '11763777465_11d01c34ce_o.jpg', 'm']
     test[4][0] = ['115321157@N03', '12111034286_4f5bfbacea_o.jpg', 'f']
-    print(test)
+    #print(test)
+    
 
     # join('data', 'aligned', test[0][0][0], test[0][0][1])
     return test
 
+
+def read_from_txt():
+    data = [[[]]]
+    
+    for i in range(5): #i = foldnumber
+        print(i)
+        j = 0; #j is image number in fold
+        with open("data/fold_frontal_{}_data.txt".format(i)) as tsv:
+            for line in csv.reader(tsv, dialect="excel-tab"): #You can also use delimiter="\t" rather than giving a dialect.
+                try:
+                    data[i].append([line[0], line[1], line[4]])
+                except:
+                    data[i][j][0] = 1
+                    
+                j+=1
+    print data[0][0]
+    
+
 if __name__ == '__main__':
     read_adience('bla')
+    read_from_txt()
