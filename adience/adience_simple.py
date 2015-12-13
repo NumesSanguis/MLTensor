a6f5665787f5b085c9189a49a348db2a5c623ddc
Kanji Origin <kanjiorigin@outlook.com>
2015-12-13 13:04:14 +0100

    removed some errors adience_simple.py

--- adience/adience_simple.py
+++ adience/adience_simple.py
@@ -5,11 +5,14 @@ from os.path import join
 
 data = adience_input.read_from_txt()
 
-input_images = tf.zeros([5])
-labels = tf.zeros([5])
+#input_images = tf.zeros([5])
+input_images = [None] * 5
+#labels = tf.zeros([5])
+labels = [None] * 5
 
 for i in range(5):
-    in_path = join("data", "aligned", data[0][i][0], "landmark_aligned_face.{}.{}.jpg".format(data[0][i][2], data[0][i][1]))
+    in_path = join("data", "aligned", data[0][i][0], "landmark_aligned_face.{}.{}".format(data[0][i][2], data[0][i][1]))
+    print(in_path)
     input_images[i] = tf.image.decode_jpeg(in_path,3)
     if data[0][i][3] == 'm':
         labels[i] = 0
@@ -19,7 +22,14 @@ for i in range(5):
         labels[i] = -1
 
 # Flattened image with undefined length (None) and 784-dimensional vector
-vector_size = input_images[0].height * input_images[0].width * input_images[0].channels
+print(input_images[0])
+#print(input_images[0][0].eval().shape)
+print(input_images[0].get_shape().as_list())  # TODO Why [None, None, 3]???
+
+#816x816 pixels
+#vector_size = input_images[0].height * input_images[0].width * input_images[0].channels
+vector_size = 816 * 816 * input_images[0].get_shape().as_list()[2]
+
 x = tf.placeholder("float", [None, vector_size])
 # W: weights and b: biases, initialized to 0
 W = tf.Variable(tf.zeros([vector_size,3]))
